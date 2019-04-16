c77a48b8018c81684b68d8f232631fa25b1c6472
wuxi <Light4mare>
2019-04-16 11:44:03 +0800

    生成集成类

--- router/plugin/src/main/groovy/com/svc/air/AirTransform.groovy
+++ router/plugin/src/main/groovy/com/svc/air/AirTransform.groovy
@@ -3,12 +3,16 @@ package com.svc.air
 import com.android.build.api.transform.*
 import com.android.build.gradle.internal.pipeline.TransformManager
 import com.google.common.collect.ImmutableSet
+import org.objectweb.asm.ClassVisitor
+import org.objectweb.asm.ClassWriter
+import org.objectweb.asm.MethodVisitor
+import org.objectweb.asm.Opcodes
 
 import java.util.concurrent.ConcurrentHashMap
 import java.util.jar.JarFile
 
 class AirTransform extends Transform {
-    private static final ROUTE_PREFIX = "route/module/Route_"
+    private static final ROUTE_PREFIX = "route.module.Route_"
 
     private boolean isApp
 
@@ -58,47 +62,66 @@ class AirTransform extends Transform {
 
         transformInvocation.inputs.each {
             it.directoryInputs.each { input ->
-                println("directoryInputs:__" + input.name)
+//                println("directoryInputs:__" + input.name)
                 int length = input.file.toString().length()
                 input.file.traverse { file ->
-                    String className = file.toString().substring(length);
-                    println("directoryInputs:__" + className)
+                    String className = file.toString().substring(length + 1).replace("\\", ".")
+//                    println("directoryInputs:__" + className)
                     if (isTargetClass(className)) {
                         initClasses.add(className)
                     }
-
-//                    if (it.isDirectory()) {
-////                        println "directory: " + dir + className
-//                        new File(dir + className).mkdirs();
-//                    } else {
-//
-//                    }
                 }
             }
 
             it.jarInputs.each { input ->
                 JarFile jarFile = new JarFile(input.file)
-                println("jarFile :" + input.file)
+//                println("jarFile :" + input.file)
                 jarFile.entries().each { entry ->
 //                    println ("jarInputs:__" + entry.getName())
-                    String className = entry.getName()
+                    String className = entry.getName().replace("/", ".")
                     if (isTargetClass(className)) {
                         initClasses.add(className)
                     }
                 }
             }
 
-            initClasses.each {
-                println("initClasses: " + it)
-            }
         }
+
+        generateRegister(dir, initClasses)
+
+        println "----------------2333333333333333 AirTransform finish----------------"
     }
 
-    private void generateRegister() {
+    private void generateRegister(String directory, Set<String> classes) {
+        println("initClasses.size: " + classes.size())
 
+        ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS)
+        ClassVisitor classVisitor = new ClassVisitor(Opcodes.ASM6, classWriter) {}
+        String loaderClassName = "com/air/router/RouteInitializer"
+        // 生成类
+        classVisitor.visit(Opcodes.V1_8, Opcodes.ACC_PUBLIC, loaderClassName, null, "java/lang/Object", null)
+        // 生成初始化方法
+        MethodVisitor methodVisitor = classVisitor.visitMethod(Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC, "init", "()V", null, null);
+        // 开始写方法代码
+        methodVisitor.visitCode()
+        classes.each { clazz ->
+            println("initClasses: " + clazz)
+            String clazzName = clazz.substring(0, clazz.length() - 6)
+            methodVisitor.visitMethodInsn(Opcodes.INVOKESTATIC, clazzName, "init", "()V", false);
+        }
+        // 由于前面用了COMPUTE_FRAMES，这里随便传
+        methodVisitor.visitMaxs(0, 0);
+        methodVisitor.visitInsn(Opcodes.RETURN);
+        methodVisitor.visitEnd();
+        classVisitor.visitEnd();
+
+        File pathDir = new File(directory + File.separator + "com/air/router/")
+        pathDir.mkdirs()
+        File transFile = new File(directory + File.separator + loaderClassName + ".class")
+        transFile.bytes = classWriter.toByteArray();
     }
 
-    private boolean isTargetClass(String className) {
+    private static boolean isTargetClass(String className) {
         return className.startsWith(ROUTE_PREFIX) && className.endsWith(".class")
     }
 }
